d49fe87f0152947fb49ec45c63336037b0a9c642
yves <yvesmomplais@hotmail.com>
2021-02-11 06:45:43 -0500

    adds pokemon_options method in cli

--- lib/cli.rb
+++ lib/cli.rb
@@ -8,11 +8,15 @@ end
 def prompt_for_pokemon
     puts "Enter Pokemon name."
     input = gets.strip.downcase
-    new_pokemon = Api.get_pokemon_data(input)
-    binding.pry
+    new_location = Api.get_pokemon_data(input)
+    pokemon_options(new_location)
 end
 
-
-
+def pokemon_options(location)
+    puts "What would you like to learn about #{location.name}? (Enter one of the following numbers.)"
+    puts "1. Type"
+    puts "2. National Pokedex Number"
+    puts "3. Height"
+    puts "4. Weight"
 
 end
