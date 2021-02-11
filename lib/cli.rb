9501c46b2da41c0c41149f7ac7ce16b041a761c0
yves <yvesmomplais@hotmail.com>
2021-02-11 07:04:49 -0500

    adds user_selection method in cli

--- lib/cli.rb
+++ lib/cli.rb
@@ -13,10 +13,29 @@ def prompt_for_pokemon
 end
 
 def pokemon_options(location)
-    puts "What would you like to learn about #{location.name}? (Enter one of the following numbers.)"
+    puts "What would you like to learn about #{location.name.capitalize}? (Enter one of the following numbers.)"
     puts "1. Type"
     puts "2. National Pokedex Number"
     puts "3. Height"
     puts "4. Weight"
+    user_selection(location)
+end
+
+def user_selection(location)
+    input = gets.strip
+
+    if input == "1"
+       puts "#{location.name.capitalize}'s type is #{location.type}."
+    elsif input == "2"
+       puts "#{location.name.capitalize}'s National Pokedex Number is #{location.id}."
+    elsif input == "3"
+       puts "#{location.name.capitalize}'s height is #{location.height} decimeters."
+    elsif input == "4"
+       puts "#{location.name.capitalize}'s weight is #{location.height} hectograms."
+    else
+       puts "That is not an option."
+       user_selection(location)
+    end
+end
 
 end
