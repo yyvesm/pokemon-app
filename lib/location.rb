ebff522a061a89eca39f76931dd52ef6d3e0f488
yves <yvesmomplais@hotmail.com>
2021-02-11 07:36:40 -0500

    stores locations

--- lib/location.rb
+++ lib/location.rb
@@ -2,13 +2,20 @@ class Location
 
 attr_accessor :name, :type, :id, :height, :weight
 
+@@all = []
+
 def initialize(hash)
 
 hash.each do |key, value|
     self.send("#{key}=", value) if self.respond_to?("#{key}=")
 end
 
+@@all << self
+
 end
 
+def self.all
+    @@all
+end
 
 end
