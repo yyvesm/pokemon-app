5dd230165bea511a490961d99c53d44050189260
yves <yvesmomplais@hotmail.com>
2021-02-11 04:54:51 -0500

    adds prompt_for_pokemon in cli.

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,7 +2,15 @@ class Cli
 
 def welcome
     puts "Welcome to the Pokemon App."
+    prompt_for_pokemon
 end
+
+def prompt_for_pokemon
+    puts "Enter Pokemon name."
+    input = gets.strip
+end
+
+
 
 
 end
